99b03634950d3688aa140a079409c54f163510fb
Kunlin Wen <kw9521@cs.rit.edu>
2023-10-22 17:25:32 -0400

    error handling added for checking commandline arguments in bracetopia.c and processCommandLines.c

--- bracetopia.c
+++ bracetopia.c
@@ -4,20 +4,9 @@
 #include <unistd.h>             // to use usleep()
 #include <stdlib.h>             // to use srandom(). random()
 #include <curses.h>             // Allows you to do cursor-controlled output management
+#include "processCommandLines.c"
 
-
-void printHelpMessage(){
-    printf("usage:\n"
-           "bracetopia [-h] [-t N] [-c N] [-d D] [-s S] [-v V] [-e E]\n"
-            "Option      Default   Example   Description \n"
-            "'-h'        n/a       -h        print this usage message.\n"
-            "'-t N'      900000    -t 5000   cycle delay, in microseconds.\n"
-            "'-c N'      n/a       -c4       cycle count maximum value.\n"
-            "'-d D'      15        -d    7   width and height dimension.\n"
-            "'-s S'      50        -s 30     strength of preference.\n"
-            "'-v V'      20        -v30      percent vacancies.\n"
-            "'-e E'      60        -e75      percent of braces that are Endline.\n");
-}
+extern int numOfPrintOnCycles;
 
 int main( int argc, char * argv[] ) {
 
@@ -27,64 +16,50 @@ int main( int argc, char * argv[] ) {
         return 1; 
     }
 
-    // default values, will be overridden 
-    int dimensionOfGrid = 15;
-    int strengthThreshold = 50;
-    int vacancyRate = 20;
-    int endLinePercentage = 60;
-    int numOfPrintOnCycles;
-    int sleepDelay = 900000;
-
-    unsigned int countCyclePrintMode;
-
-    // first parse by "-"
-    char *token = strtok(argv[0], "-");
-    // [c9 ] [d 10 ] [s50 ] [v 15 ] [e50]
-    while (token != NULL) {
-        char letter = token[0];
-
-        char *ptr;
-
-        // [d 10 ] will become ['10']
-        char *token2 = strtok(token, " abcdefghijklmnopqrstuvwxyz");
-        
-        // casts the the '10' into a long 10  
-        long theNumber = strtol(token2, &ptr, 10);
-        
+    int letter;
+    while(letter = getopt(argc, argv, "hd:s:v:e:c:t") != -1){
         switch(letter){
             case('h'):
-                printHelpMessage;
+                printHelpMessage();
                 break;
-            case('d'):
-                dimensionOfGrid = theNumber;
+            case('d'):          // sets dimensions
+                if(optarg){
+                    setDimensionOfGrid(optarg);
+                } else {
+                    fprintf(stderr, "Missing dimension values");
+                }
                 break;
-            case('c'):
-                // turn on PRINT MODE HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE TBD TBD TBD TBD TBD TBD
-                
-                // cycle cannot be negative 
-                if(theNumber < 0){
-                    fprintf(stderr, "Number of cycle in print mode is less than 0");
-                    return 1;
-                } 
-                countCyclePrintMode = theNumber; 
+            case('c'):          // set the cycle count amd turns on print mode
+                if(optarg){
+                    setNumOfPrintCycles(optarg);
+                }
                 break;
-            case('s'):
-                strengthThreshold = theNumber;
+            case('s'):          // sets strength threshold
+                if(optarg){
+                    setStrengthThreshold(optarg);
+                } else {
+                    fprintf(stderr, "Missing strength threshold values");
+                }
                 break;
-            case('v'):
-                vacancyRate = theNumber;
+            case('v'):          // sets vacancy rate
+                if(optarg){
+                    setVacancyRate(optarg);
+                } else {
+                    fprintf(stderr, "Missing vacancy rate values");
+                }
                 break;
-            case('e'):
-                endLinePercentage = theNumber;
-                break;
-            case('t'):
-                sleepDelay = theNumber;
+            case('e'):          // sets endline percentage
+                if(optarg){
+                    setEndlinePercentage(optarg);
+                } else {
+                    fprintf(stderr, "Missing endline percentage values");
+                }
+            case('t'):          // sets sleep delay
+                setSleepDelay(optarg);
                 break;
         }
 
-        token = strtok(NULL, "-");
     }
-
-
+    
 
 }
---
+++ processCommandLines.c
@@ -0,0 +1,105 @@
+#define _DEFAULT_SOURCE
+
+int dimensionOfGrid = 15;
+int strengthThreshold = 50;
+int vacancyRate = 20;
+int endlinePercentage = 60;
+int sleepDelay = 900000;
+int numOfPrintOnCycles;
+
+void printHelpMessage(){
+    printf("usage:\n"
+           "bracetopia [-h] [-t N] [-c N] [-d D] [-s S] [-v V] [-e E]\n"
+            "Option      Default   Example   Description \n"
+            "'-h'        n/a       -h        print this usage message.\n"
+            "'-t N'      900000    -t 5000   cycle delay, in microseconds.\n"
+            "'-c N'      n/a       -c4       cycle count maximum value.\n"
+            "'-d D'      15        -d    7   width and height dimension.\n"
+            "'-s S'      50        -s 30     strength of preference.\n"
+            "'-v V'      20        -v30      percent vacancies.\n"
+            "'-e E'      60        -e75      percent of braces that are Endline.\n");
+}
+
+/// -d D: set dimension.
+/// Sets the height and width of the grid (in characters) to D. 
+/// The simulation space is always square (D by D), and every cell is either vacant or occupied by an endline or a newline agent. 
+/// The minimum allowed dimension is 5 (for a 5x5 grid); the maximum is 39; and the default dimension is 15. 
+/// An invalid dimension is an error.
+void setDimensionOfGrid(int number){
+    // minimum allowed dimension is 5 (for a 5x5 grid); the maximum is 39
+    if (5 <= number && number <= 39) {
+        dimensionOfGrid = number;
+
+    } else {
+        fprintf(stderr, "Invalid Dimensions");
+    }
+
+/// -c N: set the cycle count.
+/// Turns on print mode, in which the program will perform N simulation cycles, printing out the state of the grid each cycle. 
+/// This mode does not use cursor-controlled output; instead, it just prints the grid directly. The value of N must be a non-negative integer; there is no default value. 
+/// A negative value is considered to be an error. 
+void setNumOfPrintCycles(int number) {
+    if (number < 0) {
+        fprintf(stderr, "Number of cycles in print mode is less than 0");
+    } else {
+        numOfPrintOnCycles = number;
+        // TBD TBD TBD: TURN ON PRINT MODE HERE
+    }
+}
+
+
+/// -s S: set strength threshold.
+/// Specifies an override (S) to the default strength threshold as a percentage expressed as an integer. 
+/// For example, a strength of 60 means that each agent will be satisfied and happy only if the proportion of its neighbors sharing their strength of preference is ≥ 60%. 
+/// Valid thresholds are in the range [1..99]; the default is 50. 
+/// An invalid threshold is an error.
+void setStrengthThreshold(int number){
+    // Valid thresholds are in the range [1..99]
+    if (1 <= number && number <= 99) {
+        strengthThreshold = number; 
+    } else {
+        fprintf(stderr, "Invalid Strength Threshold");
+    }
+}
+
+/// -v V: set vacancy rate.
+/// Specifies an override (V) to the default vacancy rate as a percentage value expressed as an integer. 
+/// The program interprets this value as the percentage of the entire space that is to be vacant, with all other cells occupied by agents. 
+/// Valid vacancy rates are [1..99]; the default is 20. 
+/// An invalid vacancy rate is an error.
+
+/// For example, a vacancy rate of 10 means that the percentage of vacant locations is 10%. For a 10x10 grid, this means 10 cells will be vacant and 90 will contain agents. 
+/// For grid dimensions that are not evenly divisible by the percentage of vacancy, use the truncated value for the number of vacant locations. 
+/// If the grid dimension is 5x5, and the vacancy rate is 10%, then the number of vacant locations would be 25/10 (or (int) (25 * 0.10)) which is 2.
+void setVacancyRate(int number){
+    if (1 <= number && number <= 99) {
+        vacancyRate = number;
+    } else {
+        fprintf(stderr, "Invalid Vacancy Rate");
+    }
+}
+
+/// -e E: set Endline percentage.
+/// Specifies an override (E) to the default proportion of e agents to n agents; this is the percentage of endline agents expressed as an integer.
+/// Remaining occupied cells will contain newline agents. Valid percentages are [1..99]; the default is 60
+/// An invalid percentage is an error
+void setEndlinePercentage(int number){
+    if (1 <= number && number <= 99) {
+        endlinePercentage = number;
+    } else {
+        fprintf(stderr, "Invalid Endline Percentage");
+    }
+}
+
+/// -t N: set the sleep delay.
+/// Specifies the delay, in microseconds (µsec), between simulation cycles when running in infinite mode; has no effect in print mode. 
+/// The value of N must be a positive integer; the default value is 900,000 µsec. 
+/// A value less than 1 is ignored (i.e., is not treated as an error).
+void setSleepDelay(int number){
+    if (number >= 1) {
+        sleepDelay = number;
+    }
+}
+
+}
+
